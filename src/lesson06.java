db34710b163e101c48b300ced0d1808b4c098d8c
Doğukan Vahit Bayrak <dogukanvahitbayrak@gmail.com>
2025-09-08 23:17:18 +0300

    add lesson6: while practical

---
+++ src/lesson06.java
@@ -0,0 +1,24 @@
+public class lesson06 {
+    public static void main(String[] args){
+
+
+        int i=1;
+
+
+        while(i<=4)
+        {
+            System.out.println("Hi"+ i);
+            i++;
+            int j=1;
+            while(j<=4)
+            {
+                System.out.println("Hello"+ j);
+                j++;
+            }
+
+        }
+        System.out.println("Bye"+ i);
+
+    }
+}
+
