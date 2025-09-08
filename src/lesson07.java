5b890ffcd37594e7f00e89d36ba52bbfda508410
Doğukan Vahit Bayrak <dogukanvahitbayrak@gmail.com>
2025-09-08 23:26:18 +0300

    add lesson07: do while  and for loop practical

---
+++ src/lesson07.java
@@ -0,0 +1,22 @@
+public class lesson07 {
+    public static void main(String[] args){
+
+
+        int i=5;
+
+
+        do
+        {
+            System.out.println("Hi"+ i);
+            i++;
+
+        }  while(i<=4);
+
+        for (int j=4;j>=4;j--){
+            System.out.println("Hi"+ j);
+
+        }
+
+    }
+}
+
