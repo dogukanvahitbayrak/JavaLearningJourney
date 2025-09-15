955ce1b9d5b0d06fabc1a7bcbf34b76e2dabc051
Doğukan Vahit Bayrak <dogukanvahitbayrak@gmail.com>
2025-09-15 17:50:29 +0300

    add lesson10_calculator: calculator in java

--- src/lesson10_calculator.java
+++ src/lesson10_calculator.java
@@ -1,20 +1,43 @@
+import java.sql.PreparedStatement;
 
 class Calculator{
 
-    public int Add(int a,int b) {
-        System.out.print("result :");
+        public int Add(int a,int b) {
+            int result =a+b;
+            System.out.println("result:"+result);
+            return a+b;
+        }
+        public int Add(int a,int b,int c)
+        {
+            int result =a+b+c;
+            System.out.println("result:"+result);
+            return a+b+c;
+        }
+        public double Add(double a,int b){
+        double result =a+b;
+            System.out.println("result:"+result);
         return a+b;
-    }
-    public int Subtract(int a,int b) {
-        System.out.print("result :");
+        }
+        public int Subtract(int a,int b) {
+        int result =a-b;
+        System.out.println("result:"+result);
         return a-b;
-    }
-    public int Multiply(int a,int b) {
-        System.out.print("result :");
+
+        }
+        public int Multiply(int a,int b) {
+        int result =a*b;
+        System.out.println("result:"+result);
         return a*b;
-    }
-    public int Divide(int a,int b) {
-        System.out.print("result :");
+        }
+        public int Multiply(int a,int b,int c){
+        int result =a*b*c;
+        System.out.println("result:"+result);
+        return a*b*c;
+        }
+
+        public int Divide(int a,int b) {
+        int result =a/b;
+        System.out.println("result:"+result);
         return a/b;
     }
 }
@@ -22,17 +45,27 @@ class Calculator{
 public class lesson10_calculator {
     public static void main(String[] args) {
 
-int a=15;
-int b=5;
+int    a=15;
+int    b=5;
+int    e=5;
+double d=1.0;
+
 Calculator c=new Calculator();
-c.Add(a,b);
-        System.out.println(c.Add(a,b));
+        c.Add(a,b);
+
+        c.Add(a,b,e);
+
+        c.Add(d,e);
+
         c.Subtract(a,b);
-        System.out.println(c.Subtract(a,b));
+
         c.Multiply(a,b);
-        System.out.println(c.Multiply(a,b));
+
+        c.Multiply(a,b,e);
+
         c.Divide(a,b);
-        System.out.println(c.Divide(a,b));
+
+
 
     }
 }
